c5109ef1ef9c00fe84a01f365173f39523af2416
giang610 <giang06gb@gmail.com>
2026-03-27 19:02:21 +0700

    Update User_Setup.h

--- User_Setup.h
+++ User_Setup.h
@@ -1,1 +1,13 @@
+// CYD2USB (ESP32-2432S028)
+#define ILI9341_DRIVER
+#define TFT_MISO 12
+#define TFT_MOSI 13
+#define TFT_SCLK 14
+#define TFT_CS   15
+#define TFT_DC    2
+#define TFT_RST  -1
+#define TOUCH_CS 33
+#define LOAD_GLCD
+#define SMOOTH_FONT
+#define SPI_FREQUENCY 40000000
 
